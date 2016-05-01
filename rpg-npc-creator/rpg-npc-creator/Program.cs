17109c8b883571a694451ad3849e58bd5832e69b
Eric Gruber <lonephoenix@gmail.com>
2016-05-01 12:06:24 -0400

    Initial Commit into C#

---
+++ rpg-npc-creator/rpg-npc-creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_npc_creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

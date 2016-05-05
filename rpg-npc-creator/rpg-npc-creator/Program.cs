0fb1375ddabed97e265d8c50237515b477bc7f3d
Eric Gruber <lonephoenix@gmail.com>
2016-05-04 22:05:59 -0400

    Remove factory. Start creating a Builder

--- rpg-npc-creator/rpg-npc-creator/Program.cs
+++ rpg-npc-creator/rpg-npc-creator/Program.cs
@@ -7,169 +7,51 @@ using System.Threading.Tasks;
 namespace rpg_npc_creator
 {
     // Using an Abstract Factory design pattern
-    public abstract class AbstractNpcFactory
+    public abstract class BuilderNpc
     {
-        public abstract stat AddStats();
-        public abstract name AddName();
-        public abstract backStory GenerateBackstory();
-    }
-    // Non-combat NPCs have different attributes than Combat NPCs
-    public class NonCombatNpcFactory: AbstractNpcFactory
-    {
-        public override stat AddStats()
+        public virtual void AddName()
         {
-            throw new NotImplementedException();
+
         }
-        public override name AddName()
+        public virtual void AddRace()
         {
-            throw new NotImplementedException();
-        }
-        public override backStory GenerateBackstory()
-        {
-            throw new NotImplementedException();
+
         }
 
-    }
-
-    // Combat NPCs are created with a different set of attributes
-    public class CombatNpcFactory : AbstractNpcFactory
-    {
-        public override name AddName()
+        public interface NPC
         {
-            throw new NotImplementedException();
-        }
-
-        public override stat AddStats()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override backStory GenerateBackstory()
-        {
-            throw new NotImplementedException();
+            name npcName { get; }
+            race npcRace { get; }
         }
     }
     
-    // Stat Class
-    public class stat
-    {
-        private string name;
-        private int value;
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-
-            set
-            {
-                name = value;
-            }
-        }
-
-        public int Value
-        {
-            get
-            {
-                return value;
-            }
-
-            set
-            {
-                this.value = value;
-            }
-        }
-    }
-
     // Class for Names
     public class name
     {
         private string firstName;
         private string lastName;
 
-        public string FirstName
+        public void setFirstName(string data)
         {
-            get
-            {
-                return firstName;
-            }
-
-            set
-            {
-                firstName = value;
-            }
+            firstName = data;
         }
-
-        public string LastName
+        public void setLastName(string data)
         {
-            get
-            {
-                return lastName;
-            }
-
-            set
-            {
-                lastName = value;
-            }
+            lastName = data;
         }
     }
 
-    // Class for Backstory
-    public class backStory
-    {
-        private Npc[] familyMembers;
-        private string homeTown;
-        private DateTime birthday;
-
-        public DateTime Birthday
-        {
-            get
-            {
-                return birthday;
-            }
-
-            set
-            {
-                birthday = value;
-            }
-        }
-
-        public string HomeTown
-        {
-            get
-            {
-                return homeTown;
-            }
-
-            set
-            {
-                homeTown = value;
-            }
-        }
-
-        public Npc[] FamilyMembers
-        {
-            get
-            {
-                return familyMembers;
-            }
-
-            set
-            {
-                familyMembers = value;
-            }
-        }
-    }
-       
     // This is an actual NPC, comprised of all the parts put together by the builder
     public class Npc
     {
         private name NpcName;
-        private backStory NpcBackStory;
-        private stat[] NpcStats;
-        
+    }
+    public class race
+    {
+        private enum Races
+        { 
+            Human, Elf, Dwarf
+        };
     }
 
     class Program
@@ -182,17 +64,11 @@ namespace rpg_npc_creator
 
             if(TypeOfNpc == "nonCombat")
             {
-                factory = new CombatNpcFactory();
-            }
-            else
-            {
-                factory = new NonCombatNpcFactory();
+                factory = new BuilderNpc();
             }
 
             // Now create the parts of an NPC
             factory.AddName();
-            factory.AddStats();
-            factory.GenerateBackstory();
         }
     }
 }

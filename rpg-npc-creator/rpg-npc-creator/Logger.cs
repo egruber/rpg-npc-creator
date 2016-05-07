dba9dc8ce936dd9b23de55aad974f22d8b55379c
Eric Gruber <lonephoenix@gmail.com>
2016-05-07 08:32:08 -0400

    Start work on default constructor

---
+++ rpg-npc-creator/rpg-npc-creator/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace rpg_npc_creator
+{
+    // The Logger class is an object that you can log messages to, and saves out any events that happen with a log level
+    class Logger
+    {
+        private string FileLocation;
+
+        public enum LogLevels
+        {
+            DEBUG,INFO,WARN,ERROR
+        }
+
+        public Logger()
+        {
+            // Default constructor
+            string NewFileLocation = DateTime.Now.ToString();
+
+            if (File.Exists("logfiles\\" + NewFileLocation))
+            {
+                // Recurisvely create new log files until one does not already exist
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}

5ade183d4eb6cec1deb18147e1b3abb35c18624d
Eric Gruber <lonephoenix@gmail.com>
2016-05-15 07:37:23 -0400

    Create Stat class file

--- rpg-npc-creator/rpg-npc-creator/Npc.cs
+++ rpg-npc-creator/rpg-npc-creator/Npc.cs
@@ -61,32 +61,4 @@ namespace rpg_npc_creator
         Stat Statistic { get; }
         Name NpcName { get; }
     }
-    // Add a stat to the NPC
-    public class Stat
-    {
-        int Value;
-
-        public void Set(int NewValue)
-        {
-            this.Value = NewValue;
-            Console.WriteLine("Stat Value is now: " + Value);
-        }
-
-        // Constructors
-        public Stat()
-        {
-            Value = 1;
-            Console.WriteLine("New Stat created with value: " + Value);
-        }
-        public Stat(int IncomingValue)
-        {
-            Value = IncomingValue;
-            Console.WriteLine("New Stat created with value: " + IncomingValue);
-        }
-    }
-    // Add an interface to a stat
-    public interface IStat
-    {
-        int Value { get; }
-    }
 }

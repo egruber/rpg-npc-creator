26e736d5384083f07f16684258a40bbb4e799bc7
Eric Gruber <lonephoenix@gmail.com>
2016-06-21 18:03:50 -0400

    Create Constructor test case Update project to handle NUnit

---
+++ rpg-npc-creator/rpg-npc-creator/NameUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using rpg_npc_creator;
+
+namespace NUnit.Framework.Tests
+{
+    class NameUnitTests
+    {
+        [TestCase("NewName", ExpectedResult = "NewName")]
+        public string CreateNewName(string Name)
+        {
+            Name NewNpc = new Name(Name);
+
+            return (NewNpc);
+        }
+    }
+}

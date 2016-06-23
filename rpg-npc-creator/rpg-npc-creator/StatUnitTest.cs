863391bd2dda2014ce6441195a29abbf92d55bbb
Eric Gruber <lonephoenix@gmail.com>
2016-06-23 18:04:03 -0400

    Implement new Stat Unit Test Update Project with new Unit Test class

---
+++ rpg-npc-creator/rpg-npc-creator/StatUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using rpg_npc_creator;
+
+namespace NUnit.Framework.Tests
+{
+    class StatUnitTest
+    {
+        [TestCase(1, ExpectedResult = 1)]
+        public int CreateNewStatWithValue(int IncomingValue)
+        {
+            Stat NewStat = new Stat(IncomingValue);
+            int NewValue = NewStat.Value;
+            return (NewValue);
+        }
+    }
+}

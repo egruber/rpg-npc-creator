0fec31fbc19bf558ba0a28bb04332517b0b0f05b
Eric Gruber <lonephoenix@gmail.com>
2016-07-27 05:57:58 -0400

    Remove comments that do not apply

--- rpg-npc-creator/rpg-npc-creator/StatUnitTest.cs
+++ rpg-npc-creator/rpg-npc-creator/StatUnitTest.cs
@@ -93,7 +93,6 @@ namespace NUnit.Framework.Tests
             Stat NewStat = new Stat();
             // Set up initial condition
             NewStat.Set(-1);
-            // Set the stat to MaxInt and it should return the value set at the start
             NewStat.Increase();
             Assert.AreEqual(0, NewStat.Value);
         }
@@ -103,7 +102,6 @@ namespace NUnit.Framework.Tests
             Stat NewStat = new Stat();
             // Set up initial condition
             NewStat.Set(-1);
-            // Set the stat to MaxInt and it should return the value set at the start
             NewStat.Decrease();
             Assert.AreEqual(-2, NewStat.Value);
         }
